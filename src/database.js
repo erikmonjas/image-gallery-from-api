28d1ac137ae53ff5ffac809ea304b763ccd18538
Erik Monjas <erik.monjas@gmail.com>
2019-05-12 21:49:36 +0200

    correct structure

--- src/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const dbURI = 'mongodb://localhost/imageGallery';
-
-mongoose
-  .connect(dbURI, { useNewUrlParser: true })
-  .then(db => console.log('Connected to MongoDB'))
-  .catch(e => console.log(e));
-
-module.exports = mongoose;
