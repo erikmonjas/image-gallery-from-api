0ae3627e467a46baaab1ca273b3466c762df5f36
Érik <erik.monjas@gmail.com>
2020-01-31 21:09:55 +0100

    fix deploy directory

---
+++ client/build/precache-manifest.74fa4d01d401a83ce94b23a19405bc55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709bab772d73d906fef61c807eeff94a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe2a056b079ee4900b15",
+    "url": "/static/css/main.a15daf3e.chunk.css"
+  },
+  {
+    "revision": "ee6ddc18d9ca392ca394",
+    "url": "/static/js/2.123d6f59.chunk.js"
+  },
+  {
+    "revision": "fe2a056b079ee4900b15",
+    "url": "/static/js/main.1b215aac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fd249fbe49086ead74a51023b6f3e751",
+    "url": "/static/media/DroidSerif-Regular.fd249fbe.woff"
+  },
+  {
+    "revision": "ae4d569b852ce60996e02e8ac8905ea8",
+    "url": "/static/media/Neutra-Text-Book.ae4d569b.woff"
+  }
+]);
